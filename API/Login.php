6294558c89f78508d7b6b6b95a994e9ff7dfcb26
Alan-Alberto12 <alanjoalberto@gmail.com>
2025-09-10 21:04:50 -0400

    Delete contact created
    Created Delete contact file, token of user and contact id to delete is needed as params. Contact is also returned in returnWithContactInfo function

--- API/Login.php
+++ API/Login.php
@@ -20,19 +20,38 @@
     $firstName = "";
     $lastName = "";
 
-    $conn = new mysqli($hostname, $username, $password, $database); 	
+    $conn = new mysqli($hostname, $username, $password, $database);
+
+    // Debugging code to see if we can connect and query the database
+//     $result = $conn->query("SELECT * FROM Users where login='john_Bob'");
+//     $rows = [];
+//     while ($row = $result->fetch_assoc()) {
+//         $rows[] = $row;
+//     }
+// sendResultInfoAsJson(json_encode($rows));
+
+
     if( $conn->connect_error ) {
 		returnWithError( $conn->connect_error );
 	}
     else {
+        var_dump($inData["login"]);
+        var_dump($inData["password"]);
         // prevents SQL injection
 		$stmt = $conn->prepare("SELECT ID,password FROM Users WHERE login=?");
+        //var_dump($stmt);
 		$stmt->bind_param("s", $inData["login"]);
 		$stmt->execute();
+        //var_dump($stmt);
 		$result = $stmt->get_result();
+        //var_dump($result);
 		$row = $result->fetch_assoc();
-        
+        //var_dump($row["password"]);
+        //var_dump($inData["password"]);
+        //var_dump($_ENV['JWT_SECRET']);
+        //var_dump($row["ID"]);
         if (password_verify($inData["password"], $row["password"])) {
+          //  var_dump("Password is valid!");
             $jwt = generateJWT($row['ID'], $_ENV['JWT_SECRET'], $hostname); 
             sendResultInfoAsJson(json_encode(["token" => $jwt]));
 		    $stmt->close();
