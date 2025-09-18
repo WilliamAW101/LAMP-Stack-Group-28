3d8fdb46e19d12c7278f43d715a1cf6369b76e33
Angelo <angelowhite871@gmail.com>
2025-09-18 19:09:27 -0400

    fixed logic errors and small bug with receiving contact_id

--- API/json.php
+++ API/json.php
@@ -26,7 +26,7 @@
         $searchResults = "";
 	    $searchCount = 0;
         
-        $stmt = $conn->prepare("SELECT first_name, last_name, email, phone FROM Contacts WHERE user_id = ? AND (first_name LIKE ? OR last_name LIKE ?)");
+        $stmt = $conn->prepare("SELECT contact_id, first_name, last_name, email, phone FROM Contacts WHERE user_id = ? AND (first_name LIKE ? OR last_name LIKE ?)");
 		$stmt->bind_param("iss", $userID, $search, $search);
 		$stmt->execute();
 		$result = $stmt->get_result();
